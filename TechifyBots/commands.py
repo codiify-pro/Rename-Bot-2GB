cd7aaef682f12c2be355e104133d596864068ed6
Techify Bots <TechifyBots@gmail.com>
2026-03-05 12:24:37 +0530

    Update commands.py

--- TechifyBots/commands.py
+++ TechifyBots/commands.py
@@ -1,7 +1,7 @@
 import random
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InputMediaPhoto
-from helper.database import tb
+from .database import tb
 from config import Config, Txt  
 
 @Client.on_message(filters.private & filters.command("start"))
@@ -18,6 +18,31 @@ async def start(client, message):
         caption=Txt.START_TXT.format(user.mention),
         reply_markup=button
     )
+
+@Client.on_message(filters.private & filters.command('viewthumb'))
+async def viewthumb(client,message):
+    thumb=await tb.get_thumbnail(message.from_user.id)
+    if thumb:
+        sent_msg=await client.send_photo(chat_id=message.chat.id,photo=thumb)
+    else:
+        sent_msg=await message.reply_text("❌ **𝖸𝗈𝗎 𝖣𝗈𝗇'𝗍 𝖧𝖺𝗏𝖾 𝖠𝗇𝗒 𝖳𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅**",quote=True)
+    await asyncio.sleep(30)
+    await sent_msg.delete()
+    await message.delete()
+
+@Client.on_message(filters.private & filters.command('delthumb'))
+async def removethumb(client,message):
+    await tb.set_thumbnail(message.from_user.id,file_id=None)
+    await message.reply_text("🗑️ **𝖳𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖣𝖾𝗅𝖾𝗍𝖾𝖽 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅𝗅𝗒**",quote=True)
+
+@Client.on_message(filters.private & filters.photo)
+async def addthumbs(client,message):
+    rd=await message.reply_text("⏳ **𝖯𝗅𝖾𝖺𝗌𝖾 𝖶𝖺𝗂𝗍...**",quote=True)
+    await tb.set_thumbnail(message.from_user.id,file_id=message.photo.file_id)
+    await rd.edit("✅ **𝖳𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖲𝖺𝗏𝖾𝖽 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅𝗅𝗒**")
+    await asyncio.sleep(30)
+    await message.delete()
+    await rd.delete()
 
 @Client.on_callback_query()
 async def cb_handler(client, query: CallbackQuery):
